870fd878b09274112c28d7abbf8b005f9eb25e69
Hung <ngochihung2471999@gmail.com>
2020-04-15 22:02:29 +0700

    .

---
+++ API/Controllers/CustomersController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BLL.Interfaces;
+using DAL.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    
+    public class CustomersController : BaseController
+    {
+        public CustomersController(IGuestLogic guestLogic) : base(guestLogic)
+        {
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        #region RepCode 200 400 401 404 500
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        #endregion
+        public IActionResult GetOrders()
+        {
+            try
+            {
+                PointOfSaleDBPassioContext db = new PointOfSaleDBPassioContext();
+                var customer = db.Customer.ToList();
+                
+                return Ok(customer);
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest("System Error:\n " + e.Message + e.ToString());
+            }
+        }
+    }
+}
